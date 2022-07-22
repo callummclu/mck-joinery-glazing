c70f58e8d1c26a997d5807b71b2cb98c02148d6b
Callum <callummcluskey100@gmail.com>
2022-07-22 18:15:50 +0100

    another minor build fix

--- admin-cms-frontend/pages/_app.tsx
+++ admin-cms-frontend/pages/_app.tsx
@@ -3,12 +3,9 @@ import type { AppProps } from 'next/app'
 import { UserProvider, useUser } from '@auth0/nextjs-auth0'
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  const { isLoading } = useUser()
-
   return (
     <UserProvider>
-      {isLoading ? "loading..." : <Component {...pageProps} />}
+      <Component {...pageProps} />
     </UserProvider>
   )
 }
