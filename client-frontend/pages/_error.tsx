23d2ed86b350cd08fa2cc4dc4043abed4f99b9ca
Callum <callummcluskey100@gmail.com>
2022-07-31 14:13:26 +0100

    added 500

---
+++ client-frontend/pages/_error.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+import styled from 'styled-components'
+import { AppShell, ContactDetails } from '../components/appShell'
+import { Footer, FooterProps } from '../components/Footer'
+import { MenuItems } from '../components/menubar'
+
+export const FiveHundred = ({homePage,contact,categories}:any) => {
+
+  const contactDetails = {
+    phone: "07857 073653",
+    email: "mckjoinery.glazing@gmail.com"
+  }
+
+  return(
+    <>
+      <AppShell {...contactDetails}>
+        <F04>
+          <h1>500 - Server Error</h1>
+          <Link href="/">
+            <a>
+              Go back home
+            </a>
+          </Link>
+        </F04>
+      </AppShell>
+    </>
+  )
+}
+
+const F04 = styled.div`
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  height:calc(100vh - 110px);
+  flex-direction:column;
+  & a{
+    color:darkblue;
+  }
+` 
+
+export default FiveHundred
